ce62b2c04d03e6938d7370f055780635974d1279
Dilip Waghmare <dilipwaghmarep15@gmail.com>
2026-07-15 10:38:56 +0530

    Add form for product submission with validation and state management

--- src/assignments/Assignment1.jsx
+++ src/assignments/Assignment1.jsx
@@ -8,6 +8,16 @@ function Assignment1() {
     const [error, setError] = useState(null);
     const [search, setSearch] = useState("");
 
+    // form data
+
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [category, setCategory] = useState("");
+    const [price, setPrice] = useState("");
+
+
+
+
     const fetchProducts = async () => {
         try {
             const response = await api.get('/products');
@@ -22,7 +32,53 @@ function Assignment1() {
         }
     };
 
-    
+    const addProducts = async (e) => {
+
+        e.preventDefault();
+
+        const error=[];
+
+        if(!title.trim())
+        {
+            return alert("title must be required");
+        }
+
+        if(!description.trim())
+        {
+            return alert("description must be required");
+        }
+
+        if(!category.trim())
+        {
+            return alert("category must be required");
+        }
+
+        if(!price.trim())
+        {
+            return alert("price must be required");
+        }
+
+        try {
+            const request = await api.post('/products/add', {
+                title: title,
+                description: description,
+                category: category,
+                price: price
+            })
+            console.log(request);
+            setProducts([...products, request.data]);
+
+            setTitle("");
+            setCategory("");
+            setDescription("");
+            setPrice("");
+            alert("product submited successfully");
+        }
+        catch (error) {
+
+        }
+    }
+
 
 
     useEffect(() => {
@@ -43,7 +99,16 @@ function Assignment1() {
     return (
         <>
             <div className="product-page">
-                <input type="text" value={search} placeholder="Search product title" onChange={(e)=>setSearch(e.target.value)}/>
+                <form onSubmit={addProducts}>
+                    <input type="text" placeholder="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+                    <input type="textarea" placeholder="description" value={description} onChange={(e) => setDescription(e.target.value)} />
+                    <input type="text" placeholder="category" value={category} onChange={(e) => setCategory(e.target.value)} />
+                    <input type="text" placeholder="price" value={price} onChange={(e) => setPrice(e.target.value)} />
+                    <button type="submit">
+                        submit
+                    </button>
+                </form>
+                <input type="text" value={search} placeholder="Search product title" onChange={(e) => setSearch(e.target.value)} />
                 {
                     filterProducts.map((product) => {
                         return (
